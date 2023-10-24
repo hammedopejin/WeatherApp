39cc8eb2f88a57cd38d2000c287d76608dc0b3d7
Hammed Opejin <hammedopejin@yahoo.com>
2023-10-23 18:25:19 -0700

    feat: WeatherModel created

--- Weather/Models/WeatherModel.swift
+++ Weather/Models/WeatherModel.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct WeatherModel: Codable {
+    let weatherDescription: String
+    let temperature: Double
+    let icon: String
+    let date: Date
+}
+
+struct WeatherForecastModel: Codable {
+    let dailyForecasts: [WeatherModel]
+}
+
+struct CurrentWeatherModel: Codable {
+    let currentWeather: WeatherModel
+    let forecast: WeatherForecastModel
+}
