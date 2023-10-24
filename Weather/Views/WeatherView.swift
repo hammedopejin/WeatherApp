2fd3817627254b4d4133137f2e979e46c905d8c8
Hammed Opejin <hammedopejin@yahoo.com>
2023-10-24 14:06:17 -0700

    feat: a little aesthetics to the main view

--- Weather/Views/WeatherView.swift
+++ Weather/Views/WeatherView.swift
@@ -9,11 +9,10 @@ import SwiftUI
 import CoreLocation
 
 struct WeatherView: View {
-    
     @ObservedObject var locationViewModel: LocationViewModel
     @State private var isRefreshing = false
-    @State private var currentWeather: CurrentWeatherModel? // Initialize with sample data
-    @State private var forecast: WeatherDataModel? // Initialize with sample data
+    @State private var currentWeather: CurrentWeatherModel?
+    @State private var forecast: WeatherDataModel?
     let dataManager: WeatherDataManager // Inject the data manager
     @State private var isSearchViewPresented = false
     @State private var searchText = ""
@@ -27,53 +26,64 @@ struct WeatherView: View {
     
     var body: some View {
         NavigationView {
-            List {
-                Text("Weather Forecast")
-                    .font(.largeTitle)
-                if let currentWeather = currentWeather {
-                    CurrentWeatherView(model: currentWeather)
-                }
-                
-                Text("5-Day Forecast")
-                    .font(.title)
-                
-                ScrollView(.horizontal) {
-                    HStack(spacing: 20) {
-                        if let forecast = forecast {
-                            ForEach(forecast.fiveDayForcast[1...], id: \.date) { forecast in
-                                ForecastCellView(forecast: forecast)
+            ScrollView {
+                LazyVStack {
+                    VStack { // Center everything in a VStack
+                        Text("Weather Forecast")
+                            .font(.largeTitle)
+                            .bold()
+                            .foregroundColor(Color.blue)
+                        
+                        if let currentWeather = currentWeather {
+                            CurrentWeatherView(model: currentWeather)
+                        }
+                        Spacer()
+                        Spacer()
+                        Text("5-Day Forecast")
+                            .font(.title)
+                            .bold()
+                            .foregroundColor(Color.blue)
+                        
+                        ScrollView(.horizontal) {
+                            HStack(spacing: 20) {
+                                if let forecast = forecast {
+                                    ForEach(forecast.fiveDayForcast[1...], id: \.date) { forecast in
+                                        ForecastCellView(forecast: forecast)
+                                        Divider()
+                                    }
+                                }
                             }
+                            .padding()
                         }
                     }
-                    .padding()
+                    .frame(maxWidth: .infinity) // Center the content in the ScrollView
+                    .searchable(text: $searchText) {
+                        // Search bar functionality can be added here
+                    }
+                    .refreshable {
+                        isRefreshing = true
+                        fetchData()
+                    }
+                    .onAppear {
+                        if locationViewModel.authorizationStatus == .notDetermined {
+                            locationViewModel.requestLocationAuthorization()
+                        } else if locationViewModel.authorizationStatus == .denied {
+                            shouldShowLocationServiceAlert = true
+                        } else {
+                            handleOnAppear()
+                        }
+                    }
+                    .sheet(isPresented: $isSearchViewPresented) {
+                        SearchResultsView(searchResults: $searchViewModel.searchResults)
+                    }
+                    
+                    if locationViewModel.authorizationStatus == .denied {
+                        Text("Location access is denied. Please enable location services in Settings.")
+                            .foregroundColor(.red)
+                            .padding()
+                    }
                 }
-            }
-            .searchable(text: $searchText) {
-                //                searchViewModel.search(searchText)
-            }
-            .refreshable {
-                // Handle refresh action here
-                isRefreshing = true
-                fetchData()
-            }
-            .onAppear {
-                if locationViewModel.authorizationStatus == .notDetermined {
-                    locationViewModel.requestLocationAuthorization()
-                } else if locationViewModel.authorizationStatus == .denied {
-                    shouldShowLocationServiceAlert = true
-                } else {
-                    handleOnAppear()
-                }
-            }
-            .sheet(isPresented: $isSearchViewPresented) {
-                SearchResultsView(searchResults: $searchViewModel.searchResults)
-            }
-            
-            if locationViewModel.authorizationStatus == .denied {
-                // Display an error message when authorization is denied
-                Text("Location access is denied. Please enable location services in Settings.")
-                    .foregroundColor(.red)
-                    .padding()
+                .background(Color(UIColor.systemBackground))
             }
         }
     }
@@ -100,9 +110,7 @@ struct WeatherView: View {
             longitude = -122.4194
         }
         
-        dataManager.fetchData(latitude: latitude, longitude: longitude) {
-            result in
-            // Handle the completion here
+        dataManager.fetchData(latitude: latitude, longitude: longitude) { result in
             switch result {
             case .success((let current, let forcast)):
                 self.currentWeather = current
@@ -112,10 +120,11 @@ struct WeatherView: View {
             case .failure(let error):
                 print(error.localizedDescription)
             }
+            
+            // Ensure that isRefreshing is set to false when the refresh is completed
+            self.isRefreshing = false
         }
     }
-    
-    
 }
 
 struct CurrentWeatherView: View {
@@ -123,8 +132,9 @@ struct CurrentWeatherView: View {
     let model: CurrentWeatherModel
     
     func kelvinToCelsius(_ kelvin: Double) -> String {
-        return String(format: "%.2f", kelvin - 273.15)
+        return String(format: "%.0f", kelvin - 273.15)
     }
+    
     func fetchWeatherIcon() {
         if let icon = model.weather.first?.icon {
             WeatherNetworkManager.fetchWeatherIcon(iconName: icon) { result in
@@ -144,35 +154,37 @@ struct CurrentWeatherView: View {
         VStack {
             Text("Today's Weather")
                 .font(.title)
+                .bold()
+                .padding(.top, 2)
             
-            Spacer()
             Text(model.weather.first?.description ?? "<Error>")
-                .font(.headline)
+                .font(.title)
+                .padding(.top, 2)
             
-            Spacer()
             Text("\(kelvinToCelsius(model.main.temp))°C")
-                .font(.system(size: 50))
+                .font(.largeTitle)
+                .bold()
+                .padding(.top, 2)
             
-            Spacer()
             if let weatherIcon = weatherIcon {
                 Image(uiImage: weatherIcon)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .frame(width: 200, height: 200)
+                    .frame(width: 100, height: 100)
             } else {
                 Image(systemName: "sun.max.fill")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .frame(width: 200, height: 200)
+                    .frame(width: 100, height: 100)
                     .onAppear {
                         fetchWeatherIcon()
                     }
             }
         }
-        .padding()
-        .onAppear {
-            fetchWeatherIcon()
-        }
+        .padding(10)
+        .background(Color.yellow.opacity(0.5))
+        .cornerRadius(10)
+        .frame(maxWidth: .infinity) // Center within the available space
     }
 }
 
@@ -181,7 +193,7 @@ struct ForecastCellView: View {
     @State private var weatherIcon: UIImage?
     
     func kelvinToCelsius(_ kelvin: Double) -> String {
-        return String(format: "%.2f", kelvin - 273.15)
+        return String(format: "%.0f", kelvin - 273.15)
     }
     
     func fetchWeatherIcon() {
@@ -202,27 +214,32 @@ struct ForecastCellView: View {
     var body: some View {
         VStack {
             Text(forecast.date, style: .date)
-                .font(.caption)
-            Spacer()
+                .font(.title)
+                .bold()
+                .padding(.top, 2)
             
             if let weatherIcon = weatherIcon {
                 Image(uiImage: weatherIcon)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .frame(width: 40, height: 40)
+                    .frame(width: 100, height: 100)
             } else {
                 Image(systemName: "sun.max.fill")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .frame(width: 40, height: 40)
+                    .frame(width: 100, height: 100)
                     .onAppear {
                         fetchWeatherIcon()
                     }
             }
             
-            Spacer()
             Text("\(kelvinToCelsius(forecast.list.first?.main.temp ?? 0))°C")
-                .font(.caption)
+                .font(.title)
+                .padding(.top, 2)
         }
+        .padding(10)
+        .background(Color.yellow.opacity(0.5))
+        .cornerRadius(10)
+        .frame(maxWidth: .infinity)
     }
 }
